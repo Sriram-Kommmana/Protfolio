52afee4b38910ef69dbbbb1905cb1b6d8720055b
Sriram-Kommmana <kommanamarirs@gmail.com>
2025-06-12 18:10:41 +0000

    Add Hello component and enhance styling with Russo One font
    - Introduced Hello component to display a greeting message.
    - Updated index.html to include Google Fonts for Russo One.
    - Modified App.css to adjust overflow property.
    - Enhanced index.css with custom styling for Russo One font.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Preloader from '../src/components/Preloader'
 import { AnimatePresence } from 'framer-motion';
 import Particles from './blocks/Backgrounds/Particles/Particles';
 import Navbar from './components/Navbar';
+import Hello from './components/Hello';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,8 +20,16 @@ function App() {
 
   return (
     <main className='h-screen w-screen overflow-hidden relative  bg-black'>
-      <Particles/>
-      <Navbar/>
+      <div className='absolute inset-0 z-0'>
+        <Particles />
+      </div>
+      <div className='relative z-10 h-screen w-screen flex flex-col items-center'>
+        <Navbar />
+        <Hello/>
+      </div>
+      
+
+
       <AnimatePresence mode='wait'>
         {
           isLoading && <Preloader/>
---
+++ src/components/Hello.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hello: React.FunctionComponent= () => {
+  return(
+    <div>
+        <h1 className='text-white russo-one-regular pt-[100px]'>HI, I'M SRIRAM</h1>
+    </div>
+  )
+};
+export default Hello;
